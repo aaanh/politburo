cc3918326f7bb5cfe57382b92fad1385cad470a9
Anh Hoang Nguyen <AnhNguyen@aaanh.com>
2025-04-17 15:34:42 -0400

    Add flag, optimize load

--- src/app/org-chart-client.tsx
+++ src/app/org-chart-client.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { OrgChart, Position } from "@/components/org-chart";
+import Image from "next/image";
 import { useState } from "react";
 
 interface OrgChartClientProps {
   initialPositions: Position[];
 }
 
-export default function OrgChartClient({ initialPositions }: OrgChartClientProps) {
+export default function OrgChartClient({
+  initialPositions,
+}: OrgChartClientProps) {
   const getAllPositionIds = (positions: Position[]): number[] => {
-    return positions.flatMap(pos => [
+    return positions.flatMap((pos) => [
       pos.id,
-      ...(pos.children ? getAllPositionIds(pos.children) : [])
+      ...(pos.children ? getAllPositionIds(pos.children) : []),
     ]);
   };
 
@@ -32,8 +35,28 @@ export default function OrgChartClient({ initialPositions }: OrgChartClientProps
   };
 
   return (
-    <div className="container mx-auto py-10 px-4">
-      <h1 className="text-2xl font-bold mb-6">Government of the Socialist Republic of Vietnam</h1>
+    <div className="mx-auto px-4 py-10 container">
+      <div className="flex flex-wrap items-center gap-4 mb-6">
+        <h1 className="font-bold text-2xl">
+          Government of the Socialist Republic of Vietnam
+        </h1>
+
+        <svg
+          width="50"
+          height="33.5"
+          viewBox="0 0 30 20"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          className="rounded-xl"
+        >
+          <rect width="30" height="20" fill="#da251d" />
+          <polygon
+            points="15,4 11.47,14.85 20.71,8.15 9.29,8.15 18.53,14.85"
+            fill="#ff0"
+          />
+        </svg>
+      </div>
+
       <OrgChart
         positions={initialPositions}
         expandedPositions={expandedPositions}
@@ -41,4 +64,4 @@ export default function OrgChartClient({ initialPositions }: OrgChartClientProps
       />
     </div>
   );
-} 
+}
