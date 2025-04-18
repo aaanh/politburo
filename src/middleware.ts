164d02000592060f1c3629c355dc32c55945af05
Anh Nguyen <anhnguyen@aaanh.com>
2025-04-17 20:42:04 -0400

    i18n

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,22 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { i18nRouter } from "next-i18n-router";
+import i18nConfig from "./i18nConfig";
 
 export function middleware(request: NextRequest) {
   // Check if we're in production
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     // Check if the request is for the /admin path
-    if (request.nextUrl.pathname === '/admin') {
+    if (request.nextUrl.pathname === "/admin") {
       // Redirect to home page
-      return NextResponse.redirect(new URL('/', request.url))
+      return NextResponse.redirect(new URL("/", request.url));
     }
   }
 
-  return NextResponse.next()
+  return i18nRouter(request, i18nConfig);
 }
 
 // Configure which paths the middleware should run on
 export const config = {
-  matcher: '/admin',
-} 
+  matcher: ["/admin", "/((?!api|static|.*\\..*|_next).*)"],
+};
